217a57c529b96db81078620e380ebe80d5c08da3
romarayken <romankhlystun.pl@gmail.com>
2023-11-03 21:18:04 +0100

    first commit

---
+++ operatory.py
@@ -0,0 +1,12 @@
+user_data = int(input("Введите число:"))
+
+isHappy = False
+
+if isHappy:
+    print("User is Happy")
+elif user_data == 5:
+    print("Number is 5")
+elif user_data == 7:
+    print("Number is 7")
+else:
+    print("User is unHappy")
